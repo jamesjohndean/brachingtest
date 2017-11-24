195e8e748839262980883339648339aa00050d87
jamesjohndean <jamesd@internode.on.net>
2017-11-24 10:58:45 +1030

    commiting model on feature-model branch

---
+++ src/java/model/User.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ *
+ * @author James Dean | Yak IQ
+ */
+public class User {
+
+   private int userID;
+
+   /**
+    * @return the userID
+    */
+   public int getUserID() {
+      return userID;
+   }
+
+   /**
+    * @param userID the userID to set
+    */
+   public void setUserID(int userID) {
+      this.userID = userID;
+   }
+
+}
